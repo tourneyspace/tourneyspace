1c29dfca82f99c949b5441a6ad3439a5f132bb03
= <cristian.rauchwerger@gmail.com>
2019-11-12 11:14:39 +0100

    LandingPage: Added empty landingPage

---
+++ fe/src/Components/Landingpage/LandingPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import empty from "./empty.svg";
+import './LandingPage.css'
+
+class LandingPage extends React.Component {
+    render() {
+        return <section>
+            <h1>Upcoming events</h1>
+            <div></div>
+        </section>
+    }
+}
+
+export default LandingPage;
