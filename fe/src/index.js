84dcbcdf807be7950e54d84aca5a8929abbbb052
Johannes <johannes1.riese@gmail.com>
2019-11-12 10:01:18 +0100

    add structure & first styles

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default function Body() {
+    return (
+        <main>
+            <h1>Hello World <i class="fas fa-basketball-ball"></i></h1>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+            <div className="container-example">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+
+        </main>
+    );
+}
+
 serviceWorker.unregister();
+
+// ========================================
+
+ReactDOM.render(
+    <Body />,
+    document.getElementById('root')
+);
