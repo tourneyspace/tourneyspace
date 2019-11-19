444df9fce6df655a7f772009bb580f97493ec35f
Johannes <johannes1.riese@gmail.com>
2019-11-19 16:15:42 +0100

    adds create tournament

--- fe/src/index.js
+++ fe/src/index.js
@@ -4,12 +4,13 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 // import Inventory from './Components/Inventory/Inventory';
 // import Login from './Components/Login/Login';
-import Register from './Components/Register/Register';
+// import Register from './Components/Register/Register';
+import CreateTournament from './Components/CreateTournament/CreateTournament';
 
 export default function Body() {
     return (
         <main>
-            <Register />
+            <CreateTournament />
         </main>
     );
 }
