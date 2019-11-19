0fc63c8f74712650d1aa1a00380c1e7f75274c1a
= <cristian.rauchwerger@gmail.com>
2019-11-19 08:59:57 +0100

    //WIP

--- fe/src/Components/Landingpage/LandingPage.js
+++ fe/src/Components/Landingpage/LandingPage.js
@@ -4,10 +4,32 @@ import './LandingPage.css'
 
 class LandingPage extends React.Component {
     render() {
-        return <section>
-            <h1>Upcoming events</h1>
-            <div></div>
-        </section>
+        let tourneys = Map;
+        if (tourneys == null){
+            return <section>
+                <h1>Upcoming events</h1>
+                <p>No upcoming events</p>
+                <div>
+                    <img src={empty} alt={empty}/>
+                </div>
+            </section>
+        }else{
+            return <section>
+                <h1>Upcoming events</h1>
+                <ul className="cards">
+                    <li className="cards_item">
+                        <div className="card">
+                            <div className="card_content">
+                                <h2 className="card_title">Card Grid Layout</h2>
+                                <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+                                    layout</p>
+                                <button className="btn card_btn">Read More</button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+        }
     }
 }
 
