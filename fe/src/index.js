b509f9c06f0edc859a7064140b1cab50a9097bb4
Johannes <johannes1.riese@gmail.com>
2019-11-26 11:13:50 +0100

    add edit form

--- fe/src/index.js
+++ fe/src/index.js
@@ -9,8 +9,9 @@ import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
 import Landingpage from './Components/Landingpage/LandingPage';
 import CreateTournament from './Components/CreateTournament/CreateTournament';
-import CreateTeam from './Components/CreateTeam/CreateTeam';
+import EditTournament from './Components/EditTournament/EditTournament';
 import EventOverview from "./Components/Eventoverview/EventOverview";
+import CreateTeam from './Components/CreateTeam/CreateTeam';
 
 export default function Body() {
     return (
@@ -21,6 +22,7 @@ export default function Body() {
                         <Route path='/register' component={Register} name="Register" />
                         <Route path='/landingpage' component={Landingpage} name="Landingpage" />  
                         <Route path='/createTournament' component={CreateTournament} name="CreateTournament" />
+                        <Route path='/editTournament' component={EditTournament} name="EditTournament" />
                         <Route path='/createTeam' component={CreateTeam} name="CreateTeam" />
                         <Route path='/eventOverview' component={EventOverview} name="EventOverview" />
                         {/* must be on the last position */}
