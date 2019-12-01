255535d3cb12151dd89380a218fcb8546519f0d4
Cristian Rauchwerger <cristian.rauchwerger@unic.com>
2019-12-01 17:37:39 +0100

    //WIP

--- fe/src/Components/Eventoverview/Schedule.js
+++ fe/src/Components/Eventoverview/Schedule.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './EventOverview.css'
 import nodata from './no_data.png'
+import {map} from "mongoose";
 
 class Schedule extends React.Component {
     render() {
+        let hours = new Date().getHours();
+        let minutes = new Date().getHours();
+        let currentTime = hours + ":" + minutes;
+        let gameTime = String;
         let x = 1;
-        if (x === 0){
+        if (x === 0) {
             return <div id="schedule-container">
                 <h3>Schedule</h3>
                 <img className="nodata" src={nodata} alt='none'/>
@@ -15,8 +20,14 @@ class Schedule extends React.Component {
             <h3>Schedule</h3>
             <ul className="list">
                 <li className="list-item container-example">
-                    <p><span className="blue-container time">10:30 - 11:00</span> <span>TeamOne</span> <span>vs</span>
-                        <span>TeamTwo</span> <div className="live"> </div></p>
+                    <p className="game-container">
+                        <span className="blue-container time">10:30 - 11:00</span>
+                        <span>TeamOne</span>
+                        <span className="vs">vs</span>
+                        <span>TeamTwo </span>
+                        <span>Court: 3 </span>
+                        <span className="isLiveCont"><div className="live"></div> Live</span>
+                    </p>
                 </li>
             </ul>
         </div>
