4bad15303f66643c2c16df015cb6dc287689dfea
Cristian Rauchwerger <cristian.rauchwerger@unic.com>
2019-12-12 09:35:05 +0100

    //WIP

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+chmod 777 ./start.sh
 docker-compose -f backend/docker-compose.yml up -d --build
 ./mvnw -f backend/pom.xml clean package -U
 ./mvnw -f backend spring-boot:run -d
